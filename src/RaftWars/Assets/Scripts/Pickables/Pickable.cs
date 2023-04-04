c2f6fec991cc48459d041f2cdc376441d6ac8f46
Dmitry <kliman.chan@gmail.com>
2023-04-04 20:01:49 +0300

    Скорость игрока увеличена на 54% Враги больше не могут ничего подбирать

--- src/RaftWars/Assets/Scripts/Pickables/Pickable.cs
+++ src/RaftWars/Assets/Scripts/Pickables/Pickable.cs
@@ -21,6 +21,9 @@ namespace RaftWars.Pickables
         {
             if (other.GetComponent<ICanTakePlatform>() == null)
                 return;
+                
+            if(other.gameObject.transform.parent.TryGetComponent<Enemy>(out _))
+                return;
 
             if (canTake == false)
                 return;
