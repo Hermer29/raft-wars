47a0533c627d2ef6b9def8e6aa2ecb965f532787
hermer29 <kliman.chan@gmail.com>
2023-04-28 16:11:14 +0300

    feat: танки теперь стреляют, изменен UI в меню случайной награды, босс спавнится 20 секунд

---
+++ src/RaftWars/Assets/Editor/LevelSelector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Editor
+{
+    [CreateAssetMenu]
+    public class LevelSelector : ScriptableObject
+    {
+        [SerializeField] private int _level;
+        
+        #pragma warning disable
+        
+        [NaughtyAttributes.Button]
+        public void Jump()
+        { 
+            PlayerPrefs.SetInt("Level", _level);
+        }
+        
+        #pragma warning restore
+    }
+}
