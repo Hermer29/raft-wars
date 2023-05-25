1ed2ca784b8ec6335bcbd435c5bb01a94a4311a9
hermer29 <kliman.chan@gmail.com>
2023-05-25 18:24:15 +0300

    feat: теперь статы уезжают влево если экран слишком низкий

---
+++ src/RaftWars/Assets/Scripts/Visual/StatsLayoutController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Visual
+{
+    [ExecuteAlways]
+    public class StatsLayoutController : MonoBehaviour
+    {
+        [SerializeField] private LayoutElement _stats;
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private RectTransform _transformToMove;
+        [SerializeField] private float _initialXPosition;
+        [SerializeField] private float _offsetXPosition = -500;
+        
+        private void Update()
+        {
+            if (_rectTransform.rect.height < _stats.preferredHeight)
+            {
+                _transformToMove.anchoredPosition = new Vector2(_initialXPosition + _offsetXPosition, _transformToMove.anchoredPosition.y);
+            }
+            else
+            {
+                _transformToMove.anchoredPosition = new Vector2(_initialXPosition, _transformToMove.anchoredPosition.y);
+            }
+        }
+    }
+}
