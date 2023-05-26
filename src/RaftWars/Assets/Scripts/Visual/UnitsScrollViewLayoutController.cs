c74efbe5a6306f6d3431704c2dfcf3f5f52b5f1b
hermer29 <kliman.chan@gmail.com>
2023-05-26 23:37:24 +0300

    feat: положением списка юнитов управляет layout контроллер

---
+++ src/RaftWars/Assets/Scripts/Visual/UnitsScrollViewLayoutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Visual
+{
+    [ExecuteAlways]
+    public class UnitsScrollViewLayoutController : MonoBehaviour
+    {
+        [SerializeField] private VerticalLayoutGroup _layoutGroup;
+        [SerializeField] private int _paddingTop;
+        [SerializeField] private float _borderingAspectRatio;
+        
+        private void Update()
+        {
+            var aspectRatio = Screen.width / Screen.height;
+            Debug.Log(aspectRatio);
+            if (aspectRatio < _borderingAspectRatio)
+            {
+                _layoutGroup.padding.top = _paddingTop;
+                return;
+            }
+            _layoutGroup.padding.top = 0;
+        }
+    }
+}
